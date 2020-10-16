b35f0e9428260556304d2ad31dc6a9db94587de4
Pawan96 <lohana.pawan@gmail.com.com>
2020-10-16 21:05:16 +1100

    Designed a UI which consumes a webservice for adding two numbers.

---
+++ public/env.js
@@ -0,0 +1,13 @@
+console.log('ENV loaded');
+$(document).ready(function () {
+  console.log('Document Ready');
+  $('#calculatebtn').click(function () {
+    var number1 = $('#number1').val();
+    var number2 = $('#number2').val();
+    var numbers = {num1: number1, num2: number2};
+    $.get("/adder", numbers, function (data) {
+      console.log('Data has returned' + data);
+      $('#result').val(data);
+    });
+  });
+});
